1da4b9e04b4a2b77fa2779a1dcbf9536e53a2a46
sertmer <scottertmer@gmail.com>
2019-10-30 20:33:14 -0600

    Build event listent to slide out log in menu

--- src/index.js
+++ src/index.js
@@ -9,5 +9,12 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 
-console.log('This is the JavaScript entry file - your code begins here.');
+$( document ).ready(function() {
+  $('#log-in-button').click (function() {
+    $('#log-in-menu-js').slideToggle('slow')
+  })
+});
+
+
+
 

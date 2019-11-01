370609e026f8b3662f8d58875d8cfbfdca436226
sertmer <scottertmer@gmail.com>
2019-10-31 21:22:03 -0600

    Add displayCustomerDashboard function

--- src/index.js
+++ src/index.js
@@ -10,13 +10,15 @@ $( document ).ready(() => {
   $('#login-submit-button-js').click((e) => {
     event.preventDefault();
     if ($('#username-input').val() === 'manager' && $('#password-input').val() === 'overlook2019') {
-      displayManagerDashbard();
+      displayManagerDashboard();
+    } if ($('#password-input').val() === 'overlook2019') {
+      displayCustomerDashboard();
     } else {
       displayLoginError();
     }
   });
 
-  function displayManagerDashbard() {
+  function displayManagerDashboard() {
     $('body').html(`
     <header>
         <nav>
@@ -44,6 +46,35 @@ $( document ).ready(() => {
             </section>
         </article>
       </main>`)
+  }
+
+  function displayCustomerDashboard() {
+    $('body').html(`
+    <header>
+          <nav>
+            <h1>The Overlook Hotel</h1>
+            <section class="nav-buttons-container">
+              <button id="log-out-button-js" class="log-out-button">Log Out</button>
+            </section>
+          </nav>
+        </header>
+        <main>
+          <article class="quick-look">
+            <h2>Quick Look</h2>
+            <section class="previous-visits">
+              <h3>Previous Visits</h3>
+              <p>stay one</p>
+            </section>
+            <section class="upcoming-visits">
+              <h3>Upcoming Visits</h3>
+              <p>future stay</p>
+            </section>
+            <section class="customer-loyalty">
+                <h3>Loyalty Points</h3>
+                <p>x points</p>
+            </section>
+          </article>
+        </main>`)
   }
 
   function displayLoginError() {

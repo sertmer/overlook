cf1d2379e4372541d323aeafe43ac1d73c077794
sertmer <scottertmer@gmail.com>
2019-11-02 16:08:41 -0600

    Add missing curly

--- src/Manager.js
+++ src/Manager.js
@@ -1,9 +1,10 @@
 import Hotel from './Hotel';
 
 class Manager extends Hotel {
-  constructor(users, bookings, rooms)
+  constructor(users, bookings, rooms) {
     super(bookings, rooms)
     this.users = users
+  }
 }
 
 export default Manager;

1bfaf7f366909504dced2676868fe7de0ab357a8
sertmer <scottertmer@gmail.com>
2019-11-02 16:55:02 -0600

    Pass instantiateCustomer test

--- src/Manager.js
+++ src/Manager.js
@@ -1,9 +1,16 @@
 import Hotel from './Hotel';
+import Customer from './Customer';
 
 class Manager extends Hotel {
   constructor(users, bookings, rooms) {
     super(bookings, rooms)
     this.users = users
+  }
+
+  instantiateCustomer(name) {
+    const foundCustomer = this.users.find(user => user.name === name);
+    let customer = new Customer(foundCustomer, this.bookings, []);
+    return customer;
   }
 }
 

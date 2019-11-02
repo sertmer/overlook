462a45469c7dd88751e4f7986dfe8f0d164e97e5
sertmer <scottertmer@gmail.com>
2019-11-02 15:49:03 -0600

    Build Manager class

---
+++ src/Manager.js
@@ -0,0 +1,9 @@
+import Hotel from './Hotel';
+
+class Manager extends Hotel {
+  constructor(users, bookings, rooms)
+    super(bookings, rooms)
+    this.users = users
+}
+
+export default Manager;

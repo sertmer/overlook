1647a1b5ab44ebcddca1f149dcd866dd4d01c78c
sertmer <scottertmer@gmail.com>
2019-11-02 15:38:03 -0600

    Write tests for getMy Bookings, and calculate TotalExpenses

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -6,14 +6,152 @@ chai.use(spies);
 import Customer from '../src/Customer';
 
 let customer;
-
+let bookings;
+let rooms;
 
 beforeEach(() => {
-  customer = new Customer(1, "Leatha Ullrich", [], [])
+  bookings = [
+    {
+      id: 1572293130156,
+      userID: 1,
+      date: "2019/11/06",
+      roomNumber: 18,
+      roomServiceCharges: []
+    },
+    {
+      id: 1572293130159,
+      userID: 1,
+      date: "2019/11/12",
+      roomNumber: 8,
+      roomServiceCharges: []
+    },
+    {
+      id: 1572293130159,
+      userID: 12,
+      date: "2019/10/29",
+      roomNumber: 10,
+      roomServiceCharges: []
+    },
+    {
+      id: 1572293130159,
+      userID: 27,
+      date: "2019/11/15",
+      roomNumber: 4,
+      roomServiceCharges: []
+    },
+    {
+      id: 1572293130160,
+      userID: 16,
+      date: "2019/11/06",
+      roomNumber: 7,
+      roomServiceCharges: []
+    }
+  ];
+
+  rooms = [
+    {
+      number: 18,
+      roomType: "junior suite",
+      bidet: false,
+      bedSize: "king",
+      numBeds: 2,
+      costPerNight: 496.41
+    },
+    {
+      number: 8,
+      roomType: "junior suite",
+      bidet: false,
+      bedSize: "king",
+      numBeds: 1,
+      costPerNight: 261.26
+    },
+    {
+      number: 10,
+      roomType: "suite",
+      bidet: false,
+      bedSize: "twin",
+      numBeds: 1,
+      costPerNight: 497.64
+    },
+    {
+      number: 4,
+      roomType: "single room",
+      bidet: false,
+      bedSize: "queen",
+      numBeds: 1,
+      costPerNight: 429.44
+    },
+    {
+      number: 7,
+      roomType: "single room",
+      bidet: false,
+      bedSize: "queen",
+      numBeds: 2,
+      costPerNight: 231.46
+    },
+    {
+      number: 1,
+      roomType: "residential suite",
+      bidet: true,
+      bedSize: "queen",
+      numBeds: 1,
+      costPerNight: 358.4
+    }
+  ];
+
+  customer = new Customer(1, "Leatha Ullrich", bookings, rooms)
 })
 
 describe('Customer', () => {
   it('should be a function', () => {
     expect(Customer).to.be.a('function');
   });
+
+  describe('getMyBookings', () => {
+    it('should find their own bookings', () => {
+      chai.spy.on(customer, 'getbookings', () => {
+        return [
+          {
+            id: 1572293130156,
+            userID: 1,
+            date: "2019/11/06",
+            roomNumber: 18,
+            roomServiceCharges: []
+          },
+          {
+            id: 1572293130159,
+            userID: 1,
+            date: "2019/11/12",
+            roomNumber: 8,
+            roomServiceCharges: []
+          }
+        ]
+      })
+      expect(customer.getMyBookings()).to.deep.equal([
+        {
+          id: 1572293130156,
+          userID: 1,
+          date: "2019/11/06",
+          roomNumber: 18,
+          roomServiceCharges: []
+        },
+        {
+          id: 1572293130159,
+          userID: 1,
+          date: "2019/11/12",
+          roomNumber: 8,
+          roomServiceCharges: []
+        }
+      ])
+    })
+  })
+
+  describe('calculateTotalExpenses', () => {
+    it('should know how much it has spent', () => {
+      chai.spy.on(customer, 'calculateRevenue', () => {
+        return 757.67;
+      })
+      expect(customer.calculateTotalExpenses()).to.equal(757.67)
+    })
+  })
 })
